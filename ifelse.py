491e9b7e7328966bf7d0cf757e8bc623f8998ab3
rejohnson547 <re.johnson547@gmail.com>
2020-04-27 16:58:27 -0400

    test

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+name = 'bob'
+age = 3000
+if name == 'alice':
+    print('Hi alice')
+elif age < 12:
+    print('You are not alice kiddo.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not alice granny')
