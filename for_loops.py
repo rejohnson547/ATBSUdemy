c6e58b2c4d476e0f8a9884e274185cb4980fd126
rejohnson547 <re.johnson547@gmail.com>
2020-04-29 21:30:02 -0400

    04/29/2020

---
+++ for_loops.py
@@ -0,0 +1,16 @@
+# 04/29/2020
+
+print('my name is')
+for i in range(5):
+    print('Jimmy five Times ' + str(i))
+
+
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+
+i = 0 
+while i < 5:
+    print('Jimmy Five Times ' + str(i))
+    i = i + i
