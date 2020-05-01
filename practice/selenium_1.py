399f4c68b45ac9d9a095edb53b75be7d04989d2d
rejohnson547 <re.johnson547@gmail.com>
2020-05-01 19:38:50 -0400

    05/01/2020

--- practice/selenium_1.py
+++ practice/selenium_1.py
@@ -1,7 +1,18 @@
 # 04/29/2020
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 PATH = "/Users/robertjohnson/chromedriver"
 driver = webdriver.Chrome(PATH)
 
 driver.get('https://techwithtim.net')
+print(driver.title)
+
+search = driver.find_element_by_name('s')
+search.send_keys('test')
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
+
+driver.quit()
