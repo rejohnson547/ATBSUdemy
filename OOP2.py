acca59aa291e38443ece8b8860e67b9df52fcc09
rejohnson547 <re.johnson547@gmail.com>
2020-06-04 21:25:44 -0400

    OOP

---
+++ OOP2.py
@@ -0,0 +1,14 @@
+class Employee:
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp1 = Employee('Robert', 'Johnson', 41000)
+
+print(emp1)
