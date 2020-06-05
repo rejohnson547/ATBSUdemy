523e68e14857f97605ab92ead58f382cc2a3811c
rejohnson547 <re.johnson547@gmail.com>
2020-06-04 22:05:56 -0400

    update

--- practice_class.py
+++ practice_class.py
@@ -1,9 +1,13 @@
 class House:
+
+    num_of_people = 0
     def __init__(self, name, relation, age, gender):
         self.name = name
         self.relation = relation
         self.age = age
         self.gender = gender
+
+        House.num_of_people += 1
 
 temp = House('Tempest, Davis', 'Roommate', 24, 'Female')
 gina = House('Algene, Hines', 'Mother', 52, 'Female')
@@ -13,3 +17,4 @@ Leah = House('Leah, Watkins', 'Niece', 6, 'Female')
 lex = House('Alexis, Watkins', 'Sister', 31, 'Female')
 
 print(temp.age, temp.name)
+print(House.num_of_people)
